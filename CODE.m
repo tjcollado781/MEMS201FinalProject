dad3074bbe5ea885857f3d5cabd440cbc5bb3506
CusickM <89824440+CusickM@users.noreply.github.com>
2023-04-13 15:51:15 -0500

    made pseudocode for reading in images

--- CODE.m
+++ CODE.m
@@ -2,7 +2,18 @@ close all;
 
 %need to find a good way to import all the images
 n = 55;
+A = ones(size of each image columned, n)
+
+%use a for loop to transform and concatinate each image
 for i = i:n
     img = imread(i);
-    g = im2gray(img);
-    img
+    img = im2gray(img);
+    imgCol = img(:);
+    A = [A imgcol];
+end
+m = mean(A,z);
+A_modified = A - m;
+
+%the svd line
+[U,S,V] = svd(A_modified,'econ');
+
