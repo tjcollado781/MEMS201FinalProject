6673b647beb6f2c3afc02024f342cdd5df819906
CusickM <89824440+CusickM@users.noreply.github.com>
2023-04-16 15:09:10 -0500

    got something working I believe

--- CODE.m
+++ CODE.m
@@ -1,19 +1,30 @@
 close all;
+% I found the method for reading in images here, lets be sure to cite it in the report: https://www.mathworks.com/matlabcentral/answers/396955-read-all-images-in-directory
+images = dir('data1Cropped\*.jpg');     % this reads in everything from the data1Cropped image folder as long as that folder is in your working directory.    
 
-%need to find a good way to import all the images
-n = 55;
-A = ones(size of each image columned, n)
+%set initial values based off of the images
+n = length(images);    % Number of files found
+p = 1201*901;    % get number of pixels per image
+A = ones(p, n);     % matrix the size of each image as a column by the number of images
 
-%use a for loop to transform and concatinate each image
-for i = i:n
-    img = imread(i);
-    img = im2gray(img);
-    imgCol = img(:);
-    A = [A imgcol];
+%this is a loop to read in all of the images and put them in A
+for i = 1:n
+    current_image = images(i).name;     % get the name of the current image
+    current_image = imread(['data1Cropped\' current_image]);    % retrieve the current image from the actual folder
+    img = im2gray(current_image);   % convert each image to grayscale
+    imgCol = img(:);    % turn each grayscale image into a column
+    A(:,i) = imgCol;    % throw all the images together into A
 end
-m = mean(A,z);
-A_modified = A - m;
+
+m = mean(A,2); %take the mean of A
+
+% I couldn't figure out how to do the subtracting the mean in just one line, but this for loop works. We may want to make it more efficient later though
+for j = 1:n
+    A_modified(:,j) = A(:,j) - m(:); %modify A by subtracting the mean from each column
+end
 
 %the svd line
 [U,S,V] = svd(A_modified,'econ');
+
+
 
