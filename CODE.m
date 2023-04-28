0e22382aea3e91e83d44a0b95bdcb77205ef435b
NataliaKlim <97927809+NataliaKlim@users.noreply.github.com>
2023-04-27 23:44:57 -0500

    Smiling Face Code Working

--- CODE.m
+++ CODE.m
@@ -5,28 +5,28 @@ clc;
 
 % I found the method for reading in images here, lets be sure to cite it in the report: https://www.mathworks.com/matlabcentral/answers/396955-read-all-images-in-directory
 % Read in all images
-imagesF = dir('dataResting\*.jpg');         
+imagesR = dir('dataResting\*.jpg');         
 
 %Initial values 
-n = length(imagesF);            % Number of files found
+n = length(imagesR);            % Number of files found
 p = 1201*901;                  % Number of pixels per image
 A = ones(p, n);                % Initial matrix the size of each image as a column by the number of images
 numBasis = 12;                 % Number of basis used to reconstruct images
 
 %Loop to read in all images to matrix A
 for i = 1:n
-    current_image = imagesF(i).name;                             % Get name current image
-    current_image = imread(['data1Cropped\' current_image]);    % Retrieve the current image from folder
+    current_image = imagesR(i).name;                             % Get name current image
+    current_image = imread(['dataResting\' current_image]);    % Retrieve the current image from folder
     img = im2gray(current_image);                               % Convert image to grayscale
     imgCol = img(:);                                            % Convert image to column vector
     A(:,i) = imgCol;                                            % Put image column into matrix A
 end
 
 %Find mean of A
-mF = mean(A,2);       
+mR = mean(A,2);       
 
 %Calculate eigenvectors and eigenvalues
-[U,S,V] = svd(A - mF,'econ');
+[U,S,V] = svd(A - mR,'econ');
 
 %Create plot of singular values
 figure
@@ -38,7 +38,7 @@ hold off;
 
 %Reconstruct original images
 IMeigen = U(:, [1:numBasis])*S(1:numBasis, 1:numBasis)*V(:, [1:numBasis])'; % apply the selected basis to the images
-IM = IMeigen + mF;                                                           % add the mean back to the images
+IM = IMeigen + mR;                                                           % add the mean back to the images
 IM_2D = reshape(IM, 1201, 901, []);                                         % reshape the images
 
 %Show all the Eigenfaces
@@ -46,29 +46,31 @@ figure
 implay(IM_2D/255) 
 
 
-%Repeat for Smiling Faces
-imagesF = dir('SmilingData\*.jpg');         
+
+
+% Read in all images
+imagesS = dir('dataSmiling\*.jpg'); 
 
 %Initial values 
-n = length(imagesF);            % Number of files found
+n = length(imagesS);            % Number of files found
 p = 1201*901;                  % Number of pixels per image
 A = ones(p, n);                % Initial matrix the size of each image as a column by the number of images
 numBasis = 12;                 % Number of basis used to reconstruct images
 
 %Loop to read in all images to matrix A
 for i = 1:n
-    current_image = imagesF(i).name;                             % Get name current image
-    current_image = imread(['data1Cropped\' current_image]);    % Retrieve the current image from folder
+    current_image = imagesS(i).name;                             % Get name current image
+    current_image = imread(['dataSmiling\' current_image]);    % Retrieve the current image from folder
     img = im2gray(current_image);                               % Convert image to grayscale
     imgCol = img(:);                                            % Convert image to column vector
     A(:,i) = imgCol;                                            % Put image column into matrix A
 end
 
 %Find mean of A
-mF = mean(A,2);       
+mS = mean(A,2);       
 
 %Calculate eigenvectors and eigenvalues
-[U,S,V] = svd(A - mF,'econ');
+[U,S,V] = svd(A - mS,'econ');
 
 %Create plot of singular values
 figure
@@ -80,96 +82,10 @@ hold off;
 
 %Reconstruct original images
 IMeigen = U(:, [1:numBasis])*S(1:numBasis, 1:numBasis)*V(:, [1:numBasis])'; % apply the selected basis to the images
-IM = IMeigen + mF;                                                           % add the mean back to the images
+IM = IMeigen + mS;                                                           % add the mean back to the images
 IM_2D = reshape(IM, 1201, 901, []);                                         % reshape the images
 
 %Show all the Eigenfaces
 figure
-implay(IM_2D/255)
+implay(IM_2D/255) 
 
-
-
-
-%Repeat for Smiling Faces
-imagesF = dir('SmilingData\*.jpg');         
-
-%Initial values 
-n = length(imagesF);            % Number of files found
-p = 1201*901;                  % Number of pixels per image
-A = ones(p, n);                % Initial matrix the size of each image as a column by the number of images
-numBasis = 12;                 % Number of basis used to reconstruct images
-
-%Loop to read in all images to matrix A
-for i = 1:n
-    current_image = imagesF(i).name;                             % Get name current image
-    current_image = imread(['data1Cropped\' current_image]);    % Retrieve the current image from folder
-    img = im2gray(current_image);                               % Convert image to grayscale
-    imgCol = img(:);                                            % Convert image to column vector
-    A(:,i) = imgCol;                                            % Put image column into matrix A
-end
-
-%Find mean of A
-mF = mean(A,2);       
-
-%Calculate eigenvectors and eigenvalues
-[U,S,V] = svd(A - mF,'econ');
-
-%Create plot of singular values
-figure
-plot(diag(S));
-hold on; 
-title('Singular values of Smiling Eigenfaces');
-grid on; 
-hold off;
-
-%Reconstruct original images
-IMeigen = U(:, [1:numBasis])*S(1:numBasis, 1:numBasis)*V(:, [1:numBasis])'; % apply the selected basis to the images
-IM = IMeigen + mF;                                                           % add the mean back to the images
-IM_2D = reshape(IM, 1201, 901, []);                                         % reshape the images
-
-%Show all the Eigenfaces
-figure
-implay(IM_2D/255)
-
-
-
-%Repeat for Frowning Faces
-imagesF = dir('SmilingData\*.jpg');         
-
-%Initial values 
-n = length(imagesF);            % Number of files found
-p = 1201*901;                  % Number of pixels per image
-A = ones(p, n);                % Initial matrix the size of each image as a column by the number of images
-numBasis = 12;                 % Number of basis used to reconstruct images
-
-%Loop to read in all images to matrix A
-for i = 1:n
-    current_image = imagesF(i).name;                             % Get name current image
-    current_image = imread(['data1Cropped\' current_image]);    % Retrieve the current image from folder
-    img = im2gray(current_image);                               % Convert image to grayscale
-    imgCol = img(:);                                            % Convert image to column vector
-    A(:,i) = imgCol;                                            % Put image column into matrix A
-end
-
-%Find mean of A
-mF = mean(A,2);       
-
-%Calculate eigenvectors and eigenvalues
-[U,S,V] = svd(A - mF,'econ');
-
-%Create plot of singular values
-figure
-plot(diag(S));
-hold on; 
-title('Singular values of Frowning Eigenfaces');
-grid on; 
-hold off;
-
-%Reconstruct original images
-IMeigen = U(:, [1:numBasis])*S(1:numBasis, 1:numBasis)*V(:, [1:numBasis])'; % apply the selected basis to the images
-IM = IMeigen + mF;                                                           % add the mean back to the images
-IM_2D = reshape(IM, 1201, 901, []);                                         % reshape the images
-
-%Show all the Eigenfaces
-figure
-implay(IM_2D/255)
