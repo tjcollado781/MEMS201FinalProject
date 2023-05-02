1ce7fbee4eb040cd2dd685308bc839b2bf6fa401
CusickM <89824440+CusickM@users.noreply.github.com>
2023-05-02 11:50:21 -0500

    code

--- CODE.m
+++ CODE.m
@@ -3,7 +3,57 @@ clear;
 clc; 
 % Max Cusick, Tomas Collado, Claudia Markel, Natalia Klim
 
-% I found the method for reading in images here, lets be sure to cite it in the report: https://www.mathworks.com/matlabcentral/answers/396955-read-all-images-in-directory
+%-------------------------------------------------------------------------
+% All images
+
+% Read in all images
+imagesR = dir('data1Cropped\*.jpg');       
+
+%Initial values 
+n = length(imagesR);           % Number of files found
+p = 1201*901;                  % Number of pixels per image
+A = ones(p, n);                % Initial matrix the size of each image as a column by the number of images
+numBasis = 20;                  % Number of basis used to reconstruct images
+
+%Loop to read in all images to matrix A
+for i = 1:n
+    current_image = imagesR(i).name;                            % Get name current image
+    current_image = imread(['data1Cropped\' current_image]);     % Retrieve the current image from folder
+    img = im2gray(current_image);                               % Convert image to grayscale
+    imgCol = img(:);                                            % Convert image to column vector
+    A(:,i) = imgCol;                                            % Put image column into matrix A
+end
+
+%Find mean of A
+mR = mean(A,2);       
+
+%Calculate eigenvectors and eigenvalues
+[U,S,V] = svd(A - mR,'econ');
+
+%Create plot of singular values
+plot(diag(S));
+hold on; 
+title('Singular values of All Eigenfaces');
+grid on; 
+hold off;
+
+%plot V for first image
+plot(V(1,1:20))
+grid on
+title('V values for first reconstructed eigenface')
+
+%Reconstruct original images
+IMeigen = U(:, [1:numBasis])*S(1:numBasis, 1:numBasis)*V(:, [1:numBasis])';  % apply the selected basis to the images
+IM = IMeigen + mR;                                                           % add the mean back to the images
+IM_2D = reshape(IM, 1201, 901, []);                                          % reshape the images
+
+%Show all the Eigenfaces
+implay(IM_2D/255) 
+
+
+%-------------------------------------------------------------------------
+%Resting images
+
 % Read in all images
 imagesR = dir('dataResting\*.jpg');         
 
@@ -44,11 +94,11 @@ IM = IMeigen + mR;                                                           % a
 IM_2D = reshape(IM, 1201, 901, []);                                          % reshape the images
 
 %Show all the Eigenfaces
-figure
 implay(IM_2D/255) 
 
 
-
+%-------------------------------------------------------------------------
+% Smiling images
 
 % Read in all Smiling images
 imagesS = dir('dataSmiling\*.jpg'); 
@@ -98,11 +148,11 @@ IM = IMeigen + mS;                                                           % a
 IM_2D = reshape(IM, 1201, 901, []);                                          % reshape the images
 
 %Show all the Eigenfaces
-figure
 implay(IM_2D/255) 
 
 
-
+%-------------------------------------------------------------------------
+% Frowning images
 
 % Read in all Frowning images
 imagesF = dir('dataFrowning\*.jpg'); 
@@ -152,7 +202,6 @@ IM = IMeigen + mF;                                                           % a
 IM_2D = reshape(IM, 1201, 901, []);                                          % reshape the images
 
 %Show all the Eigenfaces
-figure
 implay(IM_2D/255) 
 
 
